8bebfaad5fd316d1558cc4dcbc08e97032d2fb04
Santhosh <ja.sathish@gmail.com>
2018-07-04 12:26:34 +0000

    table update

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,23 +10,119 @@ class App extends Component
     this.state = 
     {opportunities:
     [
-  {jobID:"6001",jobname:"Web Developer"},
-  {jobID:"6002",jobname:"Java Developer"}
+      {
+        JobCode:"6001",
+        JobType:"Front End Developer",
+        Summary:"Devlopment of Front End design with new Technology stack",
+        PrimarySkills:"ReactJS, Angular JS",
+        SecondarySkills:"Ajax,Bootstrap",
+        YearsOfExperience:"2-4 Years",
+        NumberofOpenings:"7",
+        JobLocation:"Chennai,Hyderabad"
+        },
+        {
+        JobCode:"6002",
+        JobType:"Java Developer",
+        Summary:"Responsibilities Power Programmer is an important initiative within Global Delivery to develop a team of Full Stack Developers who will be working",
+        PrimarySkills:"JAVA, J2EE",
+        SecondarySkills:"Play,Rest, Web Services, Hibernate",
+        YearsOfExperience:"3-5 Years",
+        NumberofOpenings:"5",
+        JobLocation:"Chennai"
+        },
+        {
+        JobCode:"6003",
+        JobType:"DotNet Developer",
+        Summary:"Responsibilities Power Programmer is an important initiative within Global Delivery to develop a team of Full Stack Developers who will be working",
+        PrimarySkills:"Dot net 4.5,MVC,Angular",
+        SecondarySkills:"Rest, Web Services, Node JS",
+        YearsOfExperience:"4-6 Years",
+        NumberofOpenings:"3",
+        JobLocation:"Hyderabad"
+        },
+        {
+        JobCode:"6004",
+        JobType:"FullStack Developer",
+        Summary:"Responsibilities Power Programmer is an important initiative within Global Delivery to develop a team of Full Stack Developers who will be working",
+        PrimarySkills:"Java,SpringBoot,Angular,NodeJS",
+        SecondarySkills:"Web Services, Mongo DB",
+        YearsOfExperience:"5-8 Years",
+        NumberofOpenings:"5",
+        JobLocation:"Hyderabad, Chennai"
+        }
     ]
     };
   }
- 
+  
   render() {
     return (
       <div>
      
+
      
-             <ul>
-             {this.state.opportunities.map(d => <li key={d.jobID}> <a href="#">{d.jobname}</a></li>)}
-               </ul>
+           
+             {this.state.opportunities.map(d => <table border="1" cellSpacing="3" width="500" cellPadding="2" >
+       <tr>
+         <td>Job Code
+           </td>
+           <td>{d.JobCode}
+           </td>
+         </tr>
+         <tr>
+         <td>Job Type
+           </td>
+           <td>{d.JobType}
+           </td>
+         </tr>
+         <tr>
+         <td>Summary
+           </td>
+           <td>{d.Summary}
+           </td>
+         </tr>
+         <tr>
+         <td>Primary Skills
+           </td>
+           <td>{d.PrimarySkills}
+           </td>
+         </tr>
+
+         <tr>
+         <td>Secondary Skills
+           </td>
+           <td>{d.SecondarySkills}
+           </td>
+         </tr>
+         <tr>
+         <td>Years Of Experience
+           </td>
+           <td>{d.YearsOfExperience}
+           </td>
+         </tr>
+         <tr>
+         <td>Number of Openings
+           </td>
+           <td>{d.NumberofOpenings}
+           </td>
+         </tr>
+         <tr>
+         <td>JobLocation
+           </td>
+           <td>{d.JobLocation}
+           </td>
+         </tr>
+         <tr>
+         <td colSpan="1">
+         </td>
+        
+           <td><input type="button" class="go-btn" value="Apply Now"/>
+           </td>
+         </tr>
+       </table>)}
+        </div>      
              
       
-           </div>
+           
     );
   }
   
